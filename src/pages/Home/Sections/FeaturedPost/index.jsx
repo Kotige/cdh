b25d2a24b20ba3343b7ea10ac0c8800182ff2642
Vitor <vitor_fernandes_pie@yahoo.com.br>
2025-12-23 09:41:53 -0300

    style: featured post on home page desgin

---
+++ src/pages/Home/Sections/FeaturedPost/index.jsx
@@ -0,0 +1,37 @@
+import "./styles.scss"
+
+export default function FeaturedPost({ title, date, author, excerpt, link }) {
+    return (
+        <section id="featured-post"
+            className="relative w-full h-[50vh] bg-gray-100 p-4 md:p-8 flex flex-col justify-between"
+        >
+            {/* Cabeçalho */}
+            <div className="text-center">
+                <h3 className="text-2xl md:text-3xl mb-8">{title}</h3>
+                <h4 className="whitespace-nowrap">
+                    Postado em {date} por {author}.
+                </h4>
+            </div>
+
+            {/* Texto do post */}
+            <div className="relative flex-1 mt-8 overflow-hidden">
+                <div className="h-full overflow-hidden prose prose-sm md:prose-base text-justify indent-8">
+                    {excerpt}
+                </div>
+
+                {/* Gradiente mais intenso no final do texto */}
+                <div className="absolute bottom-0 left-0 w-full h-46 bg-gradient-to-t from-gray-100 to-transparent pointer-events-none"></div>
+            </div>
+
+            {/* Botão "Continue lendo" */}
+            <div className="absolute bottom-4 left-4 z-10">
+                <a
+                    href={link}
+                    className="px-4 py-2 bg-brown-800 text-black font-semibold rounded shadow-lg hover:bg-brown-900 transition-colors"
+                >
+                    Continue lendo
+                </a>
+            </div>
+        </section>
+    );
+}
--- src/pages/Home/Sections/index.jsx
+++ src/pages/Home/Sections/index.jsx
@@ -1,13 +1,32 @@
-// HomeBody.jsx
-import React from "react";
 import "./styles.scss";
+import FeaturedPost from "./FeaturedPost";
 
 export default function Sections() {
     return (
         <div className="home-sections">
             {/* Coluna esquerda */}
             <div className="column left-column">
-                <div className="content">Conteúdo da Coluna Esquerda</div>
+                <div className="content">
+                    <FeaturedPost
+                        title={"O estranho mundo da programação"}
+                        date={"16 de Setembro"}
+                        author={"Vítor Barra"}
+                        excerpt={
+                            <>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis assumenda labore esse dignissimos, quia fugit ratione iste a nisi vel fugiat provident, harum ab quod, non quisquam aut vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti natus non autem unde amet. Nobis, neque. Magni in amet consectetur error dolorum ducimus, repudiandae, suscipit delectus molestias deserunt voluptatum debitis!</p>
+                            </>
+                        }
+                        link={"algum link"}
+                    />
+                </div>
             </div>
 
             {/* Coluna direita */}
